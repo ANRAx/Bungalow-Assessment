37e5a57b3efc18b4aea30b002e27d0a315d522c0
ANRAx <ramdassaj@gmail.com>
2020-02-02 16:58:28 -0500

    make page responsive w/ media queries

--- bungalow/src/components/Card.vue
+++ bungalow/src/components/Card.vue
@@ -1,7 +1,9 @@
 <template>
     <div class="flex-grid">
         <div class="card" style="width: 18rem;" v-for="property in properties" v-bind:key=property.id>
-            <img v-bind:src="property.images && property.images[0] && property.images[0].md_url" class="card-image">
+            <img
+             class="card-image"
+             v-bind:src="property.images && property.images[0] && property.images[0].md_url || 'https://cdn.pixabay.com/photo/2017/07/28/23/18/coming-soon-2550190_960_720.jpg'">
             <div class="card-body">
                 <p class="card-text">{{property.headline}}</p>
                 <p class="card-text">{{property.available_room_count}} of {{property.total_room_count}} rooms available</p>
@@ -73,6 +75,8 @@ export default {
         .card {
             flex: 0 1 24%
         }
+
+        
     }
 
 
